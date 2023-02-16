bf6e7a798ef3943f9a1db520aaf65778facbe980
HaydenLeDev <loicroth.pro@gmail.com>
2023-02-16 14:22:19 +0100

    form

--- src/components/form/validateForm/validateForm.js
+++ src/components/form/validateForm/validateForm.js
@@ -1,8 +1,15 @@
+import { useState } from "react"
 import "./ValidateForm.scss"
 
-const ValidateFrom = ({isValidate}) => {
+const ValidateFrom = ({validate}) => {
+    const [isValidate, setIsValidate] = useState(validate)
     const classValidateForm =  isValidate ? "validateForm validateForm_actif" : "validateForm"
     
+    const onClick = () => {
+        setIsValidate(false)
+    }
+
+
     return(
         <div className={classValidateForm}>
             <div className="validateForm_v">
@@ -10,7 +17,7 @@ const ValidateFrom = ({isValidate}) => {
             </div>
             <div className="validateForm_info">
                 <p>Employee create !</p>
-                <button className="validateForm_info_close"><i className="material-icons">close</i></button>
+                <button onClick={onClick} className="validateForm_info_close"><i className="material-icons">close</i></button>
             </div>
         </div>
     )
--- src/pages/newEmployee/NewEmployee.js
+++ src/pages/newEmployee/NewEmployee.js
@@ -1,11 +1,15 @@
+import { useState } from "react"
 import Form from "../../components/form/Form"
 import ValidateFrom from "../../components/form/validateForm/ValidateForm"
 
 const NewEmployee = () => {
+
+    const [validate, setValidate] = useState(true)
+
     return(
         <main>
             <Form />
-            <ValidateFrom isValidate={false}/>
+            <ValidateFrom validate={validate} setValidate={setValidate}/>
         </main>
     )
 }
