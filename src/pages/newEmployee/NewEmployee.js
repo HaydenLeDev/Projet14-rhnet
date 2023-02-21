6df2502c9b49f7d89b0972c9a45bf0e5e7d21a1d
HaydenLeDev <loicroth.pro@gmail.com>
2023-02-21 16:56:38 +0100

    lib ok

--- src/pages/newEmployee/NewEmployee.js
+++ src/pages/newEmployee/NewEmployee.js
@@ -1,6 +1,8 @@
 import { useState } from "react"
 import Form from "../../components/form/Form"
 import ValidateFrom from "../../components/form/validateForm/ValidateForm"
+import Modal  from 'hayden-react-rhnet-library'
+import 'hayden-react-rhnet-library/dist/index.css'
 
 const NewEmployee = () => {
 
@@ -9,7 +11,7 @@ const NewEmployee = () => {
     return(
         <main>
             <Form isValidate={isValidate} setIsValidate={setIsValidate}/>
-            <ValidateFrom isValidate={isValidate} setIsValidate={setIsValidate} autoClose={true}/>
+            <Modal isValidate={isValidate} setIsValidate={setIsValidate} autoClose={true}/>
         </main>
     )
 }
